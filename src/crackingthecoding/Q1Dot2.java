3e75ba7cddf5f4be9fe9eb5224569a1ead6bbeae
OT <dev@leadwayglobal.com>
2016-07-23 08:29:18 -0400

    Added recursive method

--- src/crackingthecoding/Q1Dot2.java
+++ src/crackingthecoding/Q1Dot2.java
@@ -12,13 +12,15 @@ public class Q1Dot2 {
 		String testString = "123A";
 		System.out.println(testString);
 		System.out.println(String.valueOf(reverse(testString.toCharArray())));
-		
+		System.out.println(reverse(testString));
+
 		testString = "";
 		System.out.println(testString);
 		System.out.println(String.valueOf(reverse(testString.toCharArray())));
 
 	}
 
+	// with new array
 	public static char[] reverse(char[] str) {
 
 		int size = str.length;
@@ -29,6 +31,16 @@ public class Q1Dot2 {
 		}
 
 		return newStr;
+	}
+
+	// recursion
+	public static String reverse(String str) {
+		int length = str.length();
+		if (length <= 0)
+			return "";
+		else {
+			return str.substring(length - 1, length) + reverse(str.substring(0, length - 1));
+		}
 	}
 
 }
