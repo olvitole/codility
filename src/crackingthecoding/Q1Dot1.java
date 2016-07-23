cdf53ddcf81f30dd2f751f969a594c78e16f8a87
OT <dev@leadwayglobal.com>
2016-07-22 23:28:18 -0400

    Q1Dot1 Solution

--- src/crackingthecoding/Q1Dot1.java
+++ src/crackingthecoding/Q1Dot1.java
@@ -7,10 +7,15 @@ public class Q1Dot1 {
 		String testString = "123A";
 		System.out.println(testString);
 		System.out.println(hasUniqueCharacters(testString));
+		System.out.println(isUniqueChars2(testString));
+		System.out.println(isUniqueChars(testString));
 
 		testString = "123AA";
 		System.out.println(testString);
 		System.out.println(hasUniqueCharacters(testString));
+		System.out.println(isUniqueChars2(testString));
+		System.out.println(isUniqueChars(testString));
+
 
 	}
 
@@ -31,6 +36,34 @@ public class Q1Dot1 {
 
 		return true;
 
+	}
+
+	public static boolean isUniqueChars2(String str) {
+		if (str.length() > 256)
+			return false;
+
+		boolean[] char_set = new boolean[256];
+		for (int i = 0; i < str.length(); i++) {
+			int val = str.charAt(i);
+			if (char_set[val]) { // Already found this char in string
+				return false;
+			}
+			char_set[val] = true;
+		}
+		return true;
+	}
+
+	public static boolean isUniqueChars(String str) {
+
+		int checker = 0;
+		for (int i = 0; i < str.length(); i++) {
+			int val = str.charAt(i) - 'a';
+			if ((checker & (1 << val)) > 0) {
+				return false;
+			}
+			checker |= (1 << val);
+		}
+		return true;
 	}
 
 }
