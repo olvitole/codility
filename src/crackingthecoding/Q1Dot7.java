974ebd51e03a36426ebc5063b5d72457f9ff36c8
OT <dev@leadwayglobal.com>
2016-07-24 11:13:54 -0400

    Q1Dot7 set row col to zeros

--- src/crackingthecoding/Q1Dot7.java
+++ src/crackingthecoding/Q1Dot7.java
@@ -22,9 +22,18 @@ public class Q1Dot7 {
 		int matrix[][] = { { 1, 2, 3, 4 }, { 4, 5, 0, 7 }, { 7, 8, 9, 1 } };
 		System.out.println("\n Test ");
 		printMatrix(matrix);
-		System.out.println("output");
+		System.out.println("output Me");
 		int[][] cpy = zero(matrix);
 		printMatrix(cpy);
+		
+		
+		int matrix2[][] = { { 1, 2, 3, 4 }, { 4, 5, 0, 7 }, { 7, 8, 9, 1 } };
+		System.out.println("\n Test ");
+		printMatrix(matrix2);
+		System.out.println("output");
+		setZeros(matrix2);
+		printMatrix(matrix2);
+
 
 	}
 
@@ -66,6 +75,30 @@ public class Q1Dot7 {
 	}
 
 	// book solution
+
+	public static void setZeros(int[][] matrix) {
+		boolean[] row = new boolean[matrix.length];
+		boolean[] column = new boolean[matrix[0].length];
+
+		// Store the row and column index with value 0
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				if (matrix[i][j] == 0) {
+					row[i] = true;
+					column[j] = true;
+				}
+			}
+		}
+
+		// Set arr[i][j] to 0 if either row i or column j has a 0
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				if (row[i] || column[j]) {
+					matrix[i][j] = 0;
+				}
+			}
+		}
+	}
 
 	private static void printMatrix(int[][] grid) {
 
