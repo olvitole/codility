9dc6fa556aaf8332db900ab202c390465294499c
OT <dev@leadwayglobal.com>
2016-07-26 11:56:41 -0400

    Q2Dot1 Stub with question

---
+++ src/crackingthecoding/Q2Dot1.java
@@ -0,0 +1,18 @@
+package crackingthecoding;
+
+import crackingthecoding.misc.BasicSinglyLinkedList;
+
+/**
+ * Write code to remove duplicates from an unsorted linked list. FOLLOW UP How
+ * would you solve this problem if a temporary buffer is not allowed?
+ */
+
+public class Q2Dot1 {
+
+	public static void main(String args[]) {
+
+		BasicSinglyLinkedList.Node nd;
+
+	}
+
+}
