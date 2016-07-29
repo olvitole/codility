75444b03852495b5b1827bc73833240c88a8deaa
olvitole <olvitole@gmail.com>
2016-07-29 09:35:33 -0400

    Q2Dot3 Implemented delete method for node referred for delition

--- src/crackingthecoding/Q2Dot3.java
+++ src/crackingthecoding/Q2Dot3.java
@@ -18,19 +18,50 @@ public class Q2Dot3 {
 		nd.appendToTail(4);
 		nd.appendToTail(5);
 
-		print(nd);
+		System.out.println();
 		System.out.println("One: ");
-		
+		print(nd);
+		deleteNode(nd);
+		print(nd);
+
+		LinkedListNode nd2 = null;
+		System.out.println();
+		System.out.println("Two: ");
+		print(nd2);
+		deleteNode(nd2);
+		print(nd2);
+
+		LinkedListNode nd3 = new LinkedListNode(1);
+		System.out.println();
+		System.out.println("Three: ");
+		print(nd3);
+		deleteNode(nd3);
+		print(nd3);
+
+	}
+
+	public static void deleteNode(LinkedListNode nd) {
+
+		// will never occur because the requirements say we are in the middle
+		if (nd == null || nd.next == null) {
+			System.out.println("Nothing deleted invalid scenario");
+			return;
+		}
+
+		nd.data = nd.next.data;
+		nd.next = nd.next.next;
 
 	}
 
 	public static void print(LinkedListNode n) {
+		System.out.println("Printing begin: ");
 		while (n != null) {
 			System.out.print(n.data + " ");
 
 			n = n.next;
 		}
 		System.out.println();
+		System.out.println("Printing end. ");
 	}
 
 }
