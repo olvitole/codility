fdcb6f314121b38f98a3903154794889880f758d
OT <dev@leadwayglobal.com>
2016-07-29 06:45:59 -0400

    Q2Dot1 Deleting duplicates from linked list with no buffer

--- src/crackingthecoding/Q2Dot1.java
+++ src/crackingthecoding/Q2Dot1.java
@@ -1,6 +1,8 @@
 package crackingthecoding;
 
-import crackingthecoding.misc.BasicSinglyLinkedList;
+import java.util.Hashtable;
+
+import crackingthecoding.misc.Node;
 
 /**
  * Write code to remove duplicates from an unsorted linked list. FOLLOW UP How
@@ -11,8 +13,40 @@ public class Q2Dot1 {
 
 	public static void main(String args[]) {
 
-		BasicSinglyLinkedList.Node nd;
+		Node nd = new Node(1);
+		nd.appendToTail(1);
+		nd.appendToTail(2);
+		nd.appendToTail(3);
+		nd.appendToTail(4);
+		nd.appendToTail(5);
 
+		print(nd);
+		deleteDups(nd);
+		print(nd);
+	}
+
+	public static void deleteDups(Node n) {
+		Hashtable table = new Hashtable();
+		Node previous = null;
+		while (n != null) {
+			System.out.println(n.data);
+			if (table.containsKey(n.data)) {
+				previous.next = n.next;
+			} else {
+				table.put(n.data, true);
+				previous = n;
+			}
+			n = n.next;
+		}
+	}
+
+	public static void print(Node n) {
+		while (n != null) {
+			System.out.print(n.data + " ");
+
+			n = n.next;
+		}
+		System.out.println();
 	}
 
 }
---
+++ src/crackingthecoding/misc/Node.java
@@ -0,0 +1,20 @@
+package crackingthecoding.misc;
+
+public class Node {
+	public Node next = null;
+	public int data;
+
+	public Node(int d) {
+		data = d;
+	}
+
+	public void appendToTail(int d) {
+		Node end = new Node(d);
+		Node n = this;
+		while (n.next != null) {
+			n = n.next;
+		}
+		n.next = end;
+	}
+
+}
