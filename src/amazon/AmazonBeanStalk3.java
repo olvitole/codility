45205a1f746b228f20fd4ba7b271025b6ee2b800
olvitole <olvitole@gmail.com>
2016-09-28 12:18:11 -0400

    OO Question

---
+++ src/amazon/AmazonBeanStalk3.java
@@ -0,0 +1,70 @@
+package amazon;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class AmazonBeanStalk3 {
+
+	
+	/*
+	 /*
+I own a parking garage that provides valet parking service. When a customer pulls up to the entrance they are either rejected because the garage is full, or they are given a ticket they can use to collect their car, and the car is parked for them.
+
+Given a set of different parking bays (Small, Medium, Large), write a control program to accept/reject cars (also small, medium or large) as they arrive, and issue/redeem tickets.
+
+
+
+
+
+class Garage{
+
+HashMap<int,ParkingBay> parkingBays;
+
+isFull()
+issueTicketAndPark()
+
+// assign Bay
+assignBay(Car.size);
+
+// get the bay after assigning
+getBay(int);
+
+}
+
+
+class Car{
+  size;
+  CarSize getSize();
+}
+
+public enum CarSize {
+    SMALL,
+    MEDIUM,LARGE 
+}
+
+class SamllBay extends ParkingBay{
+    
+}
+
+class MediumBay extends ParkingBay{
+    
+}
+
+class LargeBay extends ParkingBay{
+    
+}
+
+class ParkingBay{
+  int id;
+  boolean open;
+-doesItFit(Car.size);
+ boolean isOpen();
+}
+
+Control -> garage
+parkCar(Car) // null if no space, or Ticket
+
+
+	 */
+	
+}
