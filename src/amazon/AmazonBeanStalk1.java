6201a275beb7e54a3632fcd40d784c66a5856269
olvitole <olvitole@gmail.com>
2016-09-28 12:48:33 -0400

    Amz Q1 final copy

--- src/amazon/AmazonBeanStalk1.java
+++ src/amazon/AmazonBeanStalk1.java
@@ -4,18 +4,15 @@ public class AmazonBeanStalk1 {
 
 	public static void main(String args[]) {
 
-		boolean rtn = isValidBST(null,Integer.MIN_VALUE, Integer.MAX_VALUE);
+		boolean rtn = isValidBinarySearchTree(null, Integer.MIN_VALUE, Integer.MAX_VALUE);
 		System.out.println(rtn);
 	}
 
-	public static boolean isValidBST(BinaryNode node, int MIN, int MAX) {
+	public static boolean isValidBinarySearchTree(BinaryNode node, int MIN, int MAX) {
 		if (node == null)
 			return true;
-		if (node.element > MIN && node.element < MAX && isValidBST(node.left, MIN, node.element)
-				&& isValidBST(node.right, node.element, MAX))
-			return true;
-		else
-			return false;
+		return node.element > MIN && node.element < MAX && isValidBinarySearchTree(node.left, MIN, node.element)
+				&& isValidBinarySearchTree(node.right, node.element, MAX);
 	}
 
 	class BinaryNode {
